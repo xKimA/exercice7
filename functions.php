f17f54ab718bb99f984a59c5e1fa1df4986c20a7
Kim Alarie <e1445023@cmaisonneuve.qc.ca>
2019-02-28 16:26:40 -0500

    ajout deffet de blur sur le texte de veille

---
+++ functions.php
@@ -0,0 +1,23 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+
+function my_theme_enqueue_styles() {
+
+$parent_style = 'twentynineteen-style'; // This is 'twentynineteen-style' for the Twenty nineteen theme.
+
+wp_enqueue_style ( $parent_style, get_template_directory_uri() .'/style.css' );
+
+wp_enqueue_style(
+'twentynineteen-style-enfant',
+get_stylesheet_directory_uri() .'/style.css',
+array(),
+filemtime( get_stylesheet_directory() .'/style.css' )
+);
+wp_enqueue_script(
+    'animation',
+    get_stylesheet_directory_uri() . '/js/animation.js',
+    array(),
+    filemtime( get_stylesheet_directory() . '/js/animation.js' )
+);
+}
+?>
